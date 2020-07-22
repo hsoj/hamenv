2b5252f173b2245d089d724ea523e2137b795d60
Joshua Brooks Bell <josh@outsideorbit.com>
2020-07-21 19:34:30 -0500

    Modified hardcoded port values in an attempt to get the solution to work.  Created initial tooling to simplify building and running within a local development environment.

---
+++ scripts/build.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+#
+#
+
+BUILD_DIR="build"
+IMAGES=(
+    "dvswitch"
+    "md380-emu"
+    "analog-bridge"
+    "mmdvm-bridge"
+)
+
+function get_images() {
+    images=$(docker images | awk '{printf "%s:%s\n", $1, $2}' | tail -n +2)
+    echo "${images[@]}"
+}
+
+function image_version_exists() {
+    image_name="${1}"
+    image_version="${2}"
+    install_chk=$(get_images | grep "${image_name}" | grep "${image_version}")
+    echo "${install_chk}"
+}
+
+for i in ${IMAGES[@]}
+do
+    build_path="${BUILD_DIR}/${i}"
+    version=$(cat "${build_path}/VERSION")
+    image_exists=$(image_version_exists "${i}" "${version}")
+    if [ -z "${image_exists}" ]
+    then
+        docker build -t ${i}:${version} "${build_path}"
+    fi
+done
---
+++ scripts/dev_run.sh
@@ -0,0 +1,87 @@
+#!/usr/bin/env bash
+#
+#
+
+BUILD_PATH="build"
+IMAGES=$(ls "${BUILD_PATH}")
+HOST=${HOST:-$(env | grep DOCKER_HOST | awk -F\: '{printf "%s\n", $2}' | sed "s|//||g")}
+ANALOG_ADDR=${ANALOG_ADDR:-${HOST}}
+EMULATOR_ADDR=${EMULATOR_ADDR:-${HOST}}
+MMDVM_ADDR=${MMDVM_ADDR:-${HOST}}
+
+if [ -f .env ]
+then
+    . .env
+fi
+
+if [ -z ${CALLSIGN} ]
+then
+    echo "Error: The CALLSIGN environment variable is not set."
+    exit 1
+fi
+
+if [ -z ${DMR_ID} ]
+then
+    echo "Error: The DMR_ID environment variable is not set."
+    exit 1
+fi
+
+if [ -z ${HOST} ]
+then
+    echo "Error: No HOST is defined either directly or via DOCKER_HOST."
+    exit 1
+fi
+
+function container_running() {
+    name="${1}"
+    version="${2}"
+    check=$(docker ps -f name=${name} | tail -n +2 | awk '{print $2}' | awk -F\: '{print $2}')
+    echo "${check}"
+}
+
+# TODO: Put the following into Ansible.
+
+# Ensure that the md380 emulator container is running
+# TODO: Add better port exposure handling.
+md380_image="md380-emu"
+md380_name=${MD380_NAME:-${md380_image}}
+md380_path="${BUILD_PATH}/${md380_image}"
+md380_version=${MD380_VERSION:-$(cat "${md380_path}/VERSION")}
+if [ -z $(container_running "${md380_name}" "${md380_version}") ]
+then
+    docker stop ${md380_name}
+    docker run -d --name ${md380_name} ${md380_image}:${md380_version}
+fi
+
+# Ensure that the MMDVM bridge container is running
+# TODO: Add better port exposure handling.
+mmdvm_image="mmdvm-bridge"
+mmdvm_name=${MMDVM_NAME:-${mmdvm_image}}
+mmdvm_path="${BUILD_PATH}/${mmdvm_image}"
+mmdvm_version=${MMDVM_VERSION:-$(cat "${mmdvm_path}/VERSION")}
+if [ -z $(container_running "${mmdvm_name}" "${mmdvm_version}") ]
+then
+    docker stop ${mmdvm_name}
+    docker run -d --name ${mmdvm_name} \
+        -e CALLSIGN=${CALLSIGN} \
+        -e DMR_ID=${DMR_ID} \
+        -e ANALOG_ADDR=${ANALOG_ADDR} \
+        ${mmdvm_image}:${mmdvm_version}
+fi
+
+# Ensure that the analog bridge container is running
+# TODO: Add better port exposure handling.
+analog_image="analog-bridge"
+analog_name=${ANALOG_NAME:-${analog_image}}
+analog_path="${BUILD_PATH}/${analog_name}"
+analog_version=${ANALOG_VERSION:-$(cat "${analog_path}/VERSION")}
+if [ -z $(container_running "${analog_name}" "${analog_version}") ]
+then
+    docker stop ${analog_name}
+    docker run -d --name ${analog_name} \
+        -e DMR_ID=${DMR_ID} \
+        -e ANALOG_ADDR=${ANALOG_ADDR} \
+        -e EMULATOR_ADDR=${EMULATOR_ADDR} \
+        -e MMDVM_ADDR=${MMDVM_ADDR} \
+        ${analog_image}:${analog_version}
+fi
