7ed44ba35b33e988022419996986f5df3238f5d1
Joshua Brooks Bell <josh@outsideorbit.com>
2020-07-21 19:34:30 -0500

    Modified hardcoded port values in an attempt to get the solution to work.  Created initial tooling to simplify building and running within a local development environment.

--- build/analog-bridge/run.sh
+++ build/analog-bridge/run.sh
@@ -6,6 +6,8 @@ PROGRAM_DIR="/opt/Analog_Bridge"
 CONFIG="/Analog_Bridge.ini"
 EMULATOR_ADDR=${EMULATOR_ADDR:-127.0.0.1}
 EMULATOR_PORT=${EMULATOR_PORT:-2470}
+ANALOG_ADDR=${ANALOG_ADDR:-127.0.0.1}
+MMDVM_ADDR=${MMDVM_ADDR:-127.0.0.1}
 PLATFORM=${PLATFORM:-amd64}
 SSID=${SSID:-00}
 
@@ -23,6 +25,8 @@ then
     cp ${CONFIG}.tmpl ${CONFIG}
     sed -i "s/{{EMULATOR_ADDR}}/${EMULATOR_ADDR}/g" ${CONFIG}
     sed -i "s/{{EMULATOR_PORT}}/${EMULATOR_PORT}/g" ${CONFIG}
+    sed -i "s/{{ANALOG_ADDR}}/${ANALOG_ADDR}/g" ${CONFIG}
+    sed -i "s/{{MMDVM_ADDR}}/${MMDVM_ADDR}/g" ${CONFIG}
     sed -i "s/{{DMR_ID}}/${DMR_ID}/g" ${CONFIG}
     sed -i "s/{{SSID}}/${DMR_ID}${SSID}/g" ${CONFIG}
     echo "Done"
