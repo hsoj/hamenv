b96d157fa639e7082dba6a4068f0a19f318782d8
Joshua Brooks Bell <josh@outsideorbit.com>
2020-07-21 19:34:30 -0500

    Modified hardcoded port values in an attempt to get the solution to work.  Created initial tooling to simplify building and running within a local development environment.

--- build/mmdvm-bridge/run.sh
+++ build/mmdvm-bridge/run.sh
@@ -3,12 +3,13 @@
 #
 
 REPEATER_ID=${REPEATER_ID:-00}
-BM_ID=${BM_ID:-${DMR_ID}${REPEATER_ID}}
 LATITUDE=${LATITUDE:-0}
 LONGITUDE=${LONGITUDE:-0}
 LOCATION=${LOCATION:-UNKNOWN}
 DESCRIPTION=${DESCRIPTION:-hamenv Bridge}
 BM_ADDR=${BM_ADDR:-3101.repeater.net}
+BM_PASSWD=${BM_PASSWD:-passw0rd}
+ANALOG_ADDR=${ANALOG_ADDR:-127.0.0.1}
 
 CONFIG=/MMDVM_Bridge.ini
 PROGRAM_PATH=/opt/MMDVM_Bridge
@@ -27,16 +28,11 @@ then
         echo "No DMR ID provided, exiting."
         exit 1
     fi
-    if [ -z ${BM_PASSWD} ]
-    then
-        echo "No password for brandmeister provided, exiting."
-        exit 1
-    fi
     # TODO: Implament an actual templating engine instead of the manual string 
     # replace.
     cp ${CONFIG}.tmpl ${CONFIG}
     sed -i "s/{{CALLSIGN}}/${CALLSIGN}/g" ${CONFIG}
-    sed -i "s/{{DMR_BM}}/${DMR_ID}${BM_ID}/g" ${CONFIG}
+    sed -i "s/{{DMR_BM}}/${DMR_ID}${REPEATER_ID}/g" ${CONFIG}
     sed -i "s/{{LATITUDE}}/${LATITUDE}/g" ${CONFIG}
     sed -i "s/{{LONGITUDE}}/${LONGITUDE}/g" ${CONFIG}
     sed -i "s/{{LOCATION}}/${LOCATION}/g"  ${CONFIG}
@@ -44,7 +40,11 @@ then
     sed -i "s/{{BM_ADDR}}/${BM_ADDR}/g" ${CONFIG}
     sed -i "s/{{BM_PASSWD}}/${BM_PASSWD}/g" ${CONFIG}
     # TODO: Modify this so that it doesn't just use the blanket config.
+    # TODO: Make the a variable?  It's not like it can be modified from the 
+    # directory that the bridge binary is executed from and has a hard coded
+    # name.
     cp DVSwitch.ini.tmpl DVSwitch.ini
+    sed -i "s/{{ANALOG_ADDR}}/${ANALOG_ADDR}/g" DVSwitch.ini
 fi
 
 ${PROGRAM_PATH}/MMDVM_Bridge ${CONFIG}
