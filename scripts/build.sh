c1eceee9c222bf5748a3b79c961f0028cefbec63
Joshua Brooks Bell <josh@outsideorbit.com>
2020-07-27 15:00:39 -0500

    Modifying the build helper script to leverage the common source script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,33 +1,10 @@
 #!/usr/bin/env bash
 #
 
-BUILD_DIR=${BUILD_DIR:-build}
+. "${BASH_SOURCE%/*}/common.sh"
+
 FORCE=${FORCE:-0}
-TARGET=""
-
-# build_docker Function that takes an image name and it's version and runs the 
-# build process for the directory within the BUILD_DIR
-function build_docker() {
-    local image_name="${1}"
-    local image_version="${2}"
-    docker build -t ${image_name}:${image_version} ${BUILD_DIR}/${image_name}
-}
-
-# get_images Function to get all of the images available on the configured 
-# Docker container host.
-function get_images() {
-    local images=$(docker images | awk '{printf "%s:%s\n", $1, $2}' | tail -n +2)
-    echo "${images[@]}"
-}
-
-# get_image_version Function to check the configured Docker host to determine 
-# if a container image with the provided name and version already exists.
-function get_image_version() {
-    local image_name="${1}"
-    local image_version="${2}"
-    local image_check=$(get_images | grep "${image_name}" | grep "${image_version}")
-    echo "${image_check}"
-}
+TARGET=${TARGET:-""}
 
 # print_help Prints out the help message and exits 0
 function print_help() {
@@ -69,9 +46,15 @@ fi
 # Iterate over the target
 for image_name in ${TARGET[@]}
 do
-    image_version=$(cat ${BUILD_DIR}/${image_name}/VERSION)
-    if [[ ${FORCE} -eq 1 ]] || [[ -z $(get_image_version ${image_name} ${image_version}) ]]
+    image_dir="${BUILD_DIR}/${image_name}"
+    image_version=$(get_image_version ${image_name})
+    image_exists=$(docker_image_version_exists ${image_name} ${image_version})
+    if [[ ${FORCE} -eq 1 ]] || [[ -z ${image_exists} ]]
     then
-        build_docker ${image_name} ${image_version}
+        # If there is a Dockerfile assume the build is for a docker image
+        if [ -f ${image_dir}/Dockerfile ]
+        then
+            docker_build_image ${image_name} ${image_version}
+        fi
     fi
 done
