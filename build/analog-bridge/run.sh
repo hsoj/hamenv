a8189a698f07da0f2a1828efea2d5c45923f4d12
Josh <josh@outsideorbit.com>
2020-07-27 17:40:22 -0500

    Removing separation of operator port and running everything on the same ports internally and public facing.  Modifying run scripts and startup helper script to allow for multiple instances be ran for an individual as well as multiple people.'

--- build/analog-bridge/run.sh
+++ build/analog-bridge/run.sh
@@ -7,12 +7,12 @@ CONFIG="/Analog_Bridge.ini"
 DVSWITCH_INI="/DVSwitch.ini"
 CALLSIGN=${CALLSIGN:-N0CALL}
 ANALOG_HOST=${ANALOG_HOST:-127.0.0.1}
-ANALOG_OP_PORT=${ANALOG_PORT:-31100}
+ANALOG_PORT=${ANALOG_PORT:-31100}
 EMULATOR_HOST=${EMULATOR_HOST:-127.0.0.1}
-EMULATOR_OP_PORT=${EMULATOR_PORT:-2470}
-MMDVM_HOST=${MMDVM_ADDR:-127.0.0.1}
-MMDVM_OP_PORT=${MMDVM_PORT:-32000}
-MOBILE_CLIENT_OP_PORT=${MOBILE_CLIENT_PORT:-51100}
+EMULATOR_PORT=${EMULATOR_PORT:-2470}
+MMDVM_HOST=${MMDVM_HOST:-127.0.0.1}
+MMDVM_PORT=${MMDVM_PORT:-32000}
+MOBILE_CLIENT_PORT=${MOBILE_CLIENT_PORT:-51100}
 PLATFORM=${PLATFORM:-amd64}
 REPEATER_ID=${REPEATER_ID:-00}
 
@@ -29,20 +29,20 @@ then
     echo -n "Configuring the analog bridge..."
     cp ${CONFIG}.tmpl ${CONFIG}
     sed -i "s/{{EMULATOR_HOST}}/${EMULATOR_HOST}/g" ${CONFIG}
-    sed -i "s/{{EMULATOR_OP_PORT}}/${EMULATOR_OP_PORT}/g" ${CONFIG}
+    sed -i "s/{{EMULATOR_PORT}}/${EMULATOR_PORT}/g" ${CONFIG}
     sed -i "s/{{ANALOG_HOST}}/${ANALOG_HOST}/g" ${CONFIG}
-    sed -i "s/{{ANALOG_OP_PORT}}/${ANALOG_OP_PORT}/g" ${CONFIG}
+    sed -i "s/{{ANALOG_PORT}}/${ANALOG_PORT}/g" ${CONFIG}
     sed -i "s/{{MMDVM_HOST}}/${MMDVM_HOST}/g" ${CONFIG}
-    sed -i "s/{{MMDVM_OP_PORT}}/${MMDVM_OP_PORT}/g" ${CONFIG}
-    sed -i "s/{{MOBILE_CLIENT_OP_PORT}}/${MOBILE_CLIENT_OP_PORT}/g" ${CONFIG}
+    sed -i "s/{{MMDVM_PORT}}/${MMDVM_PORT}/g" ${CONFIG}
+    sed -i "s/{{MOBILE_CLIENT_PORT}}/${MOBILE_CLIENT_PORT}/g" ${CONFIG}
     sed -i "s/{{DMR_ID}}/${DMR_ID}/g" ${CONFIG}
     sed -i "s/{{REPEATER_ID}}/${DMR_ID}${REPEATER_ID}/g" ${CONFIG}
     # Configure DVSwitch
     cp ${DVSWITCH_INI}.tmpl ${DVSWITCH_INI}
     sed -i "s/{{CALLSIGN}}/${CALLSIGN}/g" ${DVSWITCH_INI}
     sed -i "s/{{ANALOG_HOST}}/${ANALOG_HOST}/g" ${DVSWITCH_INI}
-    sed -i "s/{{ANALOG_OP_PORT}}/${ANALOG_OP_PORT}/g" ${DVSWITCH_INI}
-    sed -i "s/{{MMDVM_OP_PORT}}/${MMDVM_OP_PORT}/g" ${DVSWITCH_INI}
+    sed -i "s/{{ANALOG_PORT}}/${ANALOG_PORT}/g" ${DVSWITCH_INI}
+    sed -i "s/{{MMDVM_PORT}}/${MMDVM_PORT}/g" ${DVSWITCH_INI}
     # TODO: Figure out where this hardcoded value is getting set and determine 
     # the best way to avoid having to create this symlink
     ln -s ${DVSWITCH_INI} /opt/MMDVM_Bridge/DVSwitch.ini
--- scripts/start.sh
+++ scripts/start.sh
@@ -7,6 +7,21 @@
 __DIR="${BASH_SOURCE%/*}"
 . "${__DIR}/common.sh"
 
+function print_help() {
+    cat <<EOF
+${0} -[cdrHACEM]
+
+    -c, --callsign      The operator callsign
+    -d, --dmr-id        The operator DMR ID
+    -r, --repeater-id   The ID for the repeater
+    -H, --host          The container host running the containers
+    -A                  The port for the operator analog bridge container
+    -C                  The port that the operator mobile device connects to
+    -E                  The port for the operator emulator container
+    -M                  The port for the operator DMR bridge container
+EOF
+}
+
 # Gather all of the arguments passed and override any current configurations
 while (($#))
 do
@@ -28,20 +43,24 @@ do
             shift
         ;;
         -A)
-            ANALOG_OP_PORT=${2}
+            ANALOG_PORT=${2}
             shift
         ;;
         -C)
-            MOBILE_CLIENT_OP_PORT=${2}
+            MOBILE_CLIENT_PORT=${2}
             shift
         ;;
         -E)
-            EMULATOR_OP_PORT=${2}
+            EMULATOR_PORT=${2}
             shift
         ;;
         -M)
-            MMDVM_OP_PORT=${2}
+            MMDVM_PORT=${2}
             shift
+        ;;
+        -h)
+            print_help
+            exit 0
         ;;
     esac
     shift
@@ -69,19 +88,19 @@ fi
 emu_version=$(get_image_version ${EMULATOR_IMAGE})
 emu_op_name="${EMULATOR_IMAGE}_$(to_lower ${CALLSIGN})-${REPEATER_ID}"
 docker run -d --rm --name ${emu_op_name} \
-    -p ${EMULATOR_OP_PORT}:${EMULATOR_PORT}/udp \
+    -p ${EMULATOR_PORT}:${EMULATOR_PORT}/udp \
     ${EMULATOR_IMAGE}:${emu_version}
 
 # Start the MMDVM bridge
 mmdvm_version=$(get_image_version ${MMDVM_IMAGE})
 mmdvm_op_name="${MMDVM_IMAGE}_$(to_lower ${CALLSIGN})-${REPEATER_ID}"
 docker run -d --rm --name ${mmdvm_op_name} \
-    -p ${MMDVM_OP_PORT}:${MMDVM_PORT}/udp \
+    -p ${MMDVM_PORT}:${MMDVM_PORT}/udp \
     -e CALLSIGN=${CALLSIGN} \
     -e DMR_ID=${DMR_ID} \
     -e ANALOG_HOST=${HOST} \
-    -e ANALOG_OP_PORT=${ANALOG_OP_PORT} \
-    -e MMDVM_OP_PORT=${MMDVM_OP_PORT} \
+    -e ANALOG_PORT=${ANALOG_PORT} \
+    -e MMDVM_PORT=${MMDVM_PORT} \
     -e REPEATER_ID=${REPEATER_ID} \
     ${MMDVM_IMAGE}:${mmdvm_version}
 
@@ -89,14 +108,15 @@ docker run -d --rm --name ${mmdvm_op_name} \
 analog_version=$(get_image_version ${ANALOG_IMAGE})
 analog_op_name="${ANALOG_IMAGE}_$(to_lower ${CALLSIGN})-${REPEATER_ID}"
 docker run -d --rm --name ${analog_op_name} \
-    -p ${ANALOG_OP_PORT}:${ANALOG_PORT}/udp \
-    -p ${MOBILE_CLIENT_OP_PORT}:${MOBILE_CLIENT_PORT}/udp \
+    -p ${ANALOG_PORT}:${ANALOG_PORT}/udp \
+    -p ${MOBILE_CLIENT_PORT}:${MOBILE_CLIENT_PORT}/udp \
     -e CALLSIGN=${CALLSIGN} \
     -e DMR_ID=${DMR_ID} \
     -e ANALOG_HOST=${HOST} \
-    -e ANALOG_OP_PORT=${ANALOG_OP_PORT} \
+    -e ANALOG_PORT=${ANALOG_PORT} \
     -e EMULATOR_HOST=${HOST} \
-    -e EMULATOR_OP_PORT=${EMULATOR_OP_PORT} \
+    -e EMULATOR_PORT=${EMULATOR_PORT} \
     -e MMDVM_HOST=${HOST} \
-    -e MMDVM_OP_PORT=${MMDVM_OP_PORT} \
+    -e MMDVM_PORT=${MMDVM_PORT} \
+    -e MOBILE_CLIENT_PORT=${MOBILE_CLIENT_PORT} \
     ${ANALOG_IMAGE}:${analog_version}
